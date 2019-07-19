f6371d698a7685b5baa2f07759917f61ceaa5b0d
Fabien Lenoir <flenoir@gmail.com>
2019-07-19 10:15:52 +0200

    improved tests

---
+++ search/tests/test_models.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from search.models import Product
+
+
+class TestModels(TestCase):
+
+    def setUp(self):
+        self.product1 = Product.objects.create(
+            product_name="test product",
+            product_code="12345656789",
+            nutriscore="a"
+            )
+
+
+    def test_Product_is_capitalized(self):
+        self.assertEquals(self.product1.__str__(), "Test product")
+
+
+    def test_Product_is_created(self):        
+        self.assertEquals(Product.objects.all().count(), 1)
