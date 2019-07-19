1d826551e1e4f8122e4f2b4f497492ed8ee3598e
Fabien Lenoir <flenoir@gmail.com>
2019-07-19 14:20:12 +0200

    added .keep file to detect static folder

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -166,22 +166,16 @@ ACCOUNT_LOGOUT_ON_GET = True
 
 if os.environ.get('ENV') == 'PRODUCTION':
 
-    # # Static files settings
-    # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+    # Static files settings
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-    # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles/')
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-    # # Extra places for collectstatic to find static files.
-    # STATICFILES_DIRS = (
-    #     os.path.join(PROJECT_ROOT, 'static/'),
-    # )
+    # Extra places for collectstatic to find static files.
+    STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT, 'static'),
+    )
 
-
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATIC_TMP = os.path.join(BASE_DIR, 'static')
-    STATIC_URL = '/static/'
-
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
 
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
