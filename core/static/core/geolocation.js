f8311952a30649a08adbb1ecbf55e1ecfa79f26d
Fabien Lenoir <flenoir@gmail.com>
2019-11-07 16:47:45 +0100

    added retreival of customer geoloaction through browser

---
+++ core/static/core/geolocation.js
@@ -0,0 +1,27 @@
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      console.log(pos);
+      
+    //   geocoder.geocode( { 'location': pos}, function(results, status, infowindow) {
+    //   if (status == 'OK') {
+    //         console.log(results[0].formatted_address);
+    //         infoWindow.setContent('Location found: '+ results[0].formatted_address);
+    //         infoWindow.setPosition(pos);
+    //         infoWindow.open(map);
+    //   } else {
+    //   			console.log('Geocode was not successful for the following reason: ' + status);
+    //   }
+    // });
+
+    //   map.setCenter(pos);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
