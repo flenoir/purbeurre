f8e3aa4583fa94e7863f7a3cf19479e2707df566
Fabien Lenoir <flenoir@gmail.com>
2019-11-11 19:31:52 +0100

    cleaned javascript code

--- core/static/core/geolocation.js
+++ core/static/core/geolocation.js
@@ -1,31 +1,10 @@
   // Try HTML5 geolocation.
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
       var lat = document.getElementById('latitude');
       var lon = document.getElementById('longitude');
       lat.value = position.coords.latitude;
       lon.value = position.coords.longitude;
-
-
-      console.log(pos);
-   
-      
-    //   geocoder.geocode( { 'location': pos}, function(results, status, infowindow) {
-    //   if (status == 'OK') {
-    //         console.log(results[0].formatted_address);
-    //         infoWindow.setContent('Location found: '+ results[0].formatted_address);
-    //         infoWindow.setPosition(pos);
-    //         infoWindow.open(map);
-    //   } else {
-    //   			console.log('Geocode was not successful for the following reason: ' + status);
-    //   }
-    // });
-
-    //   map.setCenter(pos);
     });
   } else {
     // Browser doesn't support Geolocation
