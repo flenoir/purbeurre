70790209d9b7079391d0bc05f5be9c484ebc60ef
Fabien Lenoir <flenoir@gmail.com>
2019-11-14 10:42:58 +0100

    re merged correct geolocation.js

--- core/static/core/geolocation.js
+++ core/static/core/geolocation.js
@@ -1,12 +1,10 @@
- // Try HTML5 geolocation.
- if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var lat = document.getElementById('latitude');
-    var lon = document.getElementById('longitude');
-    lat.value = position.coords.latitude;
-    lon.value = position.coords.longitude;
-  });
-} else {
-  // Browser doesn't support Geolocation
-  handleLocationError(false, infoWindow, map.getCenter());
-}
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      sessionStorage.setItem('lat', position.coords.latitude);
+      sessionStorage.setItem('lon', position.coords.longitude);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
